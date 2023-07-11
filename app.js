48b5ed16d456e81f4baa9f8b129007441fc18915
a.khajegan <khajegan.amirhossein@mapnaec.com>
2023-07-11 08:54:38 +0330

    Move manifest file to assets folder

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ app.use(cors({
   origin: '*'
 }));
 app.get("/url", (req, res, next) => {
-  setTimeout(()=>{
+  setTimeout(() => {
     console.log('receive get request' + new Date());
-    res.json(["Tony","Lisa","Michael","Ginger","Food", "Alpha", "Beta"]);
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food", "Alpha", "Beta"]);
   }, 8000);
 });
 app.use(express.urlencoded());
 app.use(express.json());
 app.post("/url", (req, res, next) => {
-  setTimeout(()=>{
+  setTimeout(() => {
     console.log('receive post request' + new Date());
     res.json(Number(req.body.num1) + Number(req.body.num2));
   }, 8000);
